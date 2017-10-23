9c65e6953bd53eb3160031bd01fd42cdb29229cb
Alvaro Urbina <4lvaro.up@gmail.com>
2017-10-23 11:00:45 -0300

    chango

--- app/models/animal.rb
+++ app/models/animal.rb
@@ -1,3 +1,24 @@
 class Animal < ApplicationRecord
   belongs_to :animal_type
+  scope :changos, -> {where(animal_type: AnimalType.find_by(name:"Chango"))}
+  scope :tipo_animal, -> (tipo) {where(animal__type: AnimalType.find_by(name: tipo))}
+  scope :ultimos, -> {last(5)}
+
+
+  def self.changos2
+    where(animal_type: AnimalType.find_by(name:"Chango"))
+  end
+
+  def self.tipo(num)
+    where(animal__type: AnimalType.find_by(name: num))
+  end
+
+  def self.ultimos
+    last(5)
+  end
+
+  def self.trae(num)
+    last(num)
+  end
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+10.times do
+  Animal.create!(name: Faker::Pokemon.name, animal_type: AnimalType.first)
+end
+
+10.times do
+  Animal.create!(name: Faker::Pokemon.name, animal_type: AnimalType.last)
+end
